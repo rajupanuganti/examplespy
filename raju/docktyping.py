9cfa0259a5cabd59a49383109a99c162167fa6df
rajuf6 <rajupanugantif6@gmail.com>
2020-06-16 23:38:24 +0530

    first commit

---
+++ raju/docktyping.py
@@ -0,0 +1,18 @@
+class VScode:
+    def excute(self):
+        print('compiling')
+        print('running')
+
+class mycomp:
+    def excute(self):
+        print('filter')
+        print('beauty')
+        print('runnn,compiling')
+
+class Laptop():
+    def code(self,ide):
+        ide.excute()
+
+ide = mycomp()
+lap1 = Laptop()
+lap1.code(ide)
