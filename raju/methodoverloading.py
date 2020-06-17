8f9411d7b274686e4b32df4839bffbf9ad2513e2
rajuf6 <rajupanugantif6@gmail.com>
2020-06-17 22:40:04 +0530

    first commit

---
+++ raju/methodoverloading.py
@@ -0,0 +1,12 @@
+class methodover:
+    def m1(self):
+        print('firstttt')
+
+    def m1(self,a):
+        print('secondddd')
+
+    def m1(self,a,b):
+        print('rajuuuu')
+
+o = methodover()
+o.m1(11,22)
