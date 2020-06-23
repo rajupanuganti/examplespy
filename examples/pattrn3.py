c20c2c3ed6efca3785a39822a66f54fa97205a1c
rajuf6 <rajupanugantif6@gmail.com>
2020-06-23 13:49:56 +0530

    second commit

--- examples/pattrn3.py
+++ examples/pattrn3.py
@@ -1,6 +1,7 @@
-for row in range(1,5):
-    for col in range(1,8):
-        if row==4 or row+col==5 or col-row==3:
+n= int(input('enter a num of row '))
+for row in range(1,n+1):
+    for col in range(1,2*n):
+        if row==n or row+col==n+1 or col-row==n-1:
             print('*',end='')
         else:
             print(end=' ')
